84085fa2056a99f4342a9458a30ec79273c6ec59
jvandaal <jonas.vandaal@outlook.be>
2022-12-07 15:41:10 +0100

    chore: working authentication

--- samples/AcmIdmConsumer/Program.cs
+++ samples/AcmIdmConsumer/Program.cs
@@ -1,11 +1,11 @@
-using System.Security.Principal;
 using Be.Vlaanderen.Basisregisters.AcmIdm.Abstractions;
+using Be.Vlaanderen.Basisregisters.AcmIdm.Abstractions.AuthenticationHandlers;
 using Be.Vlaanderen.Basisregisters.AcmIdm.Abstractions.AuthorizationHandlers;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 var ourScope = new[] { "dv_gr_geschetstgebouw_beheer", "dv_gr_geschetstgebouw_uitzonderingen" };
@@ -18,20 +18,22 @@ var authority = "authority";
 var introspectionEndpoint = "introspectionEndpoint";
 
 builder.Services.AddSingleton<IAuthorizationHandler, RequiredScopesAuthorizationHandler>();
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer();
+builder.Services.AddSingleton<IAuthenticationHandler, CustomAuthenticationHandler>();
+builder.Services.AddAuthentication("Bearer")
+    .AddScheme<JwtBearerOptions, CustomAuthenticationHandler>("Bearer", null);
+// builder.Services.AddAuthentication(options =>
+// {
+//     options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+// }).AddJwtBearer();
 //builder.Services.AddAcmIdmAuthentication(clientId, clientSecret, authority, introspectionEndpoint);
 builder.Services.AddAcmIdmAuthorization(ourScope);
 
 var app = builder.Build();
 
 app.MapGet("/", (HttpContext httpContext) => { })
-    //.RequireAuthorization("acm-idm-scopes")
-    ;
+    .RequireAuthorization("acm-idm-scopes");
 
 app.UseAuthentication();
 app.UseAuthorization()
