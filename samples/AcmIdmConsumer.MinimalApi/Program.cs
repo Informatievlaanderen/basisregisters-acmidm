292c22f7c09e72b46223a8715c83e5d7114e49ca
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-12-13 08:00:13 +0100

    fix: fix Program

--- samples/AcmIdmConsumer.MinimalApi/Program.cs
+++ samples/AcmIdmConsumer.MinimalApi/Program.cs
@@ -10,9 +10,6 @@ namespace AcmIdmConsumer.MinimalApi
 
     public class Program
     {
-        protected Program()
-        { }
-
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
