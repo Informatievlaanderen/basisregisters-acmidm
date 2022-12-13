ef10084ab1b880e903517a0c23d6177170ebf568
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-12-13 11:12:35 +0100

    fix: cleanup

--- test/Be.Vlaanderen.Basisregisters.AcmIdm.Tests.ContainerHelper/IdentityServerFakeContainer.cs
+++ test/Be.Vlaanderen.Basisregisters.AcmIdm.Tests.ContainerHelper/IdentityServerFakeContainer.cs
@@ -7,22 +7,15 @@ namespace Be.Vlaanderen.Basisregisters.AcmIdm.Tests.ContainerHelper
 
     public static class IdentityServerFakeContainer
     {
-        public static ICompositeService Compose(TimeSpan? timeout = null)
+        public static ICompositeService Compose()
         {
             var fileName = Path.Combine(Directory.GetCurrentDirectory(), "identityserverfake_test.yml");
-
-            const string waitForService = "acmidm-test";
-            const string waitForPort = "5052";
-            const string waitForProto = "tcp";
-
-            var timeoutMs = timeout?.TotalMilliseconds ?? 30_000;
 
             return new Builder()
                 .UseContainer()
                 .UseCompose()
                 .FromFile(fileName)
                 .RemoveOrphans()
-                //.WaitForPort(waitForService, $"{waitForPort}:{waitForProto}", Convert.ToInt64(timeoutMs))
                 .Build()
                 .Start();
         }
