fe5864d28768411f3d463b4b9d56419709a9c33d
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-12-13 14:52:38 +0100

    fix: make Program static

--- samples/AcmIdmConsumer.MinimalApi/Program.cs
+++ samples/AcmIdmConsumer.MinimalApi/Program.cs
@@ -10,6 +10,9 @@ namespace AcmIdmConsumer.MinimalApi
 
     public class Program
     {
+        protected Program()
+        { }
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
