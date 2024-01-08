b9d723fa87f79b537db005600f714593bdb14d9e
jvandaal <jonas.vandaal@outlook.be>
2024-01-08 11:50:52 +0100

    feat: add HasScope and IsInterneBijwerker extensions

---
+++ test/Be.Vlaanderen.Basisregisters.Auth.Tests/ExtensionTests.cs
@@ -0,0 +1,53 @@
+﻿namespace Be.Vlaanderen.Basisregisters.Auth.Tests
+{
+    using System.Security.Claims;
+    using AcmIdm;
+    using FluentAssertions;
+    using Microsoft.AspNetCore.Http;
+    using Xunit;
+
+    public class ExtensionTests
+    {
+        private readonly DefaultHttpContext _httpContext = new()
+        {
+            User = new ClaimsPrincipal(new ClaimsIdentity(
+                new[]
+                {
+                    new Claim(AcmIdmClaimTypes.Scope, Scopes.DvArAdresUitzonderingen)
+                }))
+        };
+
+        [Fact]
+        public void HasScope()
+        {
+            _httpContext.HasScope(Scopes.DvArAdresUitzonderingen).Should().BeTrue();
+        }
+
+        [Fact]
+        public void DoesntHaveScope()
+        {
+            _httpContext.HasScope(Scopes.DvArAdresBeheer).Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsInterneBijwerker()
+        {
+            _httpContext.IsInterneBijwerker().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsNotInterneBijwerker()
+        {
+            DefaultHttpContext httpContext = new()
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(
+                    new[]
+                    {
+                        new Claim(AcmIdmClaimTypes.Scope, Scopes.DvArAdresBeheer)
+                    }))
+            };
+
+            httpContext.IsInterneBijwerker().Should().BeFalse();
+        }
+    }
+}
