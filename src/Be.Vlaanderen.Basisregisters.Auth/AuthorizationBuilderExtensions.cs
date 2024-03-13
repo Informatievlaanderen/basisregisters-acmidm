d23d088d42fe85714989d996237ca96d7ced945d
jvandaal <jonas.vandaal@outlook.be>
2024-03-13 08:21:00 +0100

    feat: move to dotnet 8.0.2
    move to dotnet 8.0.2
    
    BREAKING CHANGE: move to dotnet 8.0.2

--- src/Be.Vlaanderen.Basisregisters.Auth/AuthorizationBuilderExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Be.Vlaanderen.Basisregisters.Auth
-{
-    using Microsoft.Extensions.DependencyInjection;
-
-    public static class AuthorizationBuilderExtensions
-    {
-        public static AuthorizationBuilder AddAuthorizationBuilder(this IServiceCollection services) => new AuthorizationBuilder(services.AddAuthorization());
-    }
-}
---
+++ src/Be.Vlaanderen.Basisregisters.Auth/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Basisregisters Vlaanderen")]
+[assembly: AssemblyDescription("BasisRegisters Auth ACM/IDM utilities for C#.")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("59ab4c69-f6a6-45b0-854b-1b084edbf22c")]
